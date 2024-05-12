951dfc8484a5b721c633b7b3f817142c53e2aa9f
freyac777 <freyac666@gmail.com>
2024-05-11 21:30:11 -0500

    route53

--- route53.tf
+++ route53.tf
@@ -1,11 +1,12 @@
-resource "aws_route53_record" "test" {
-  zone_id = "your_zone_id"  # Specify the Route 53 hosted zone ID where you want to create the record
-  name    = "test.yourdomain.link"  # Specify the domain name you want to associate with the ALB
+resource "aws_route53_record" "www" {
+  zone_id = "Z08990232N81R0KC6694K" - we can create variabels to use host zone id  # Specify the Route 53 hosted zone ID where you want to create the record
+  name    = "juliamashkova.link" - we need to change to Nodar name  # Specify the domain name you want to associate with the ALB
   type    = "A"
   alias {
-    name                   = "aws_lb.wordpress_alb.dns_name"  # Specify the DNS name of your ALB
-    zone_id                = "aws_lb.wordpress_alb.zone_id"  # Specify the hosted zone ID of your ALB
+    name                   = aws_lb.wordpress_alb.dns_name  # Specify the DNS name of your ALB
+    zone_id                = aws_lb.wordpress_alb.zone_id
+    # Specify the Route 53 hosted zone ID where you want to create the record
+# "  # Specify the hosted zone ID of your ALB
     evaluate_target_health = true
   }
 }
-
